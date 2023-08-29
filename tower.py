5feed6ef4e92bb756a9ad23d0410cb28b75c40c9
FWiharso <140255381+FWiharso@users.noreply.github.com>
2023-08-30 02:05:18 +1000

    Update tower.py

--- tower.py
+++ tower.py
@@ -14,31 +14,70 @@ class BattleTower:
     MAX_LIVES = 10
 
     def __init__(self, battle: Battle|None=None) -> None:
+        """
+        O(1) - Time complexity is constant, only involves creating battle instance.
+        """
         self.battle = battle or Battle(verbosity=0)
+        self.player_team = None
+        self.tower_teams = []
 
     def set_my_team(self, team: MonsterTeam) -> None:
-        # Generate the team lives here too.
-        raise NotImplementedError
+        """
+        O(1) - Involves assigning a value based on the generated number of lives
+        It will always be O(1) regardless of the value.
+        """
+        self.player_team = team
+        self.player_team.lives = RandomGen.randint(BattleTower.MIN_LIVES, BattleTower.MAX_LIVES + 1)
 
     def generate_teams(self, n: int) -> None:
-        raise NotImplementedError
+        """
+        O(n), since it is dependent on the number of different teams generated.
+        """
+        for _ in range(n):
+            team = MonsterTeam(MonsterTeam.TeamMode.BACK, MonsterTeam.SelectionMode.RANDOM)
+            team.lives = RandomGen.randint(BattleTower.MIN_LIVES, BattleTower.MAX_LIVES + 1)
+            self.tower_teams.append(team)
 
     def battles_remaining(self) -> bool:
-        raise NotImplementedError
+        """
+        O(n), since it considers the teams assigned from the generate_teams method, analyzing the amount of lives.
+        """
+        return self.player_team.lives > 0 and any(team.lives > 0 for team in self.tower_teams)
 
-    def next_battle(self) -> tuple[Battle.Result, MonsterTeam, MonsterTeam, int, int]:
-        raise NotImplementedError
+    def __iter__(self):
+        """
+        O(1)
+        """
+        return self
+
+    def __next__(self) -> tuple[Battle.Result, MonsterTeam, MonsterTeam, int, int]:
+        """
+        O(1), since it simply creates commands to execute the battle and modifies the values in the amount of lives remaining.
+        """
+        if not self.battles_remaining():
+            raise StopIteration
+
+        tower_team = self.tower_teams.pop(0)
+        result = self.battle.battle(self.player_team, tower_team)
+        player_lives = self.player_team.lives
+        tower_lives = tower_team.lives
+
+        if result == Battle.Result.TEAM1:
+            tower_team.lives -= 1
+        elif result == Battle.Result.TEAM2:
+            self.player_team.lives -= 1
+        else:  # Draw
+            self.player_team.lives -= 1
+            tower_team.lives -= 1
+
+        return result, self.player_team, tower_team, player_lives, tower_lives
 
     def out_of_meta(self) -> ArrayR[Element]:
-        raise NotImplementedError
-
-    def sort_by_lives(self):
-        # 1054 ONLY
-        raise NotImplementedError
-
-def tournament_balanced(tournament_array: ArrayR[str]):
-    # 1054 ONLY
-    raise NotImplementedError
+        """"""
+        elements_present = set()
+        for team in self.tower_teams:
+            elements_present.update(monster.element for monster in team.monsters)
+        return ArrayR([element for element in Element if element not in elements_present])
 
 if __name__ == "__main__":
 
